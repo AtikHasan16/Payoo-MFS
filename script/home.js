b7bf2043fd110667ebdfdcf7ad584d8b9153ab12
AtikHasan16 <akhasannaeem@gmail.com>
2025-08-23 11:09:27 +0600

    added few style

--- script/home.js
+++ script/home.js
@@ -1,5 +1,33 @@
 // Log out button functionality
 document.getElementById("log-out").addEventListener("click", function (event) {
-  event.preventDefault;
+  event.preventDefault();
   window.location.replace("../login.html");
 });
+
+document
+  .getElementById("add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    // console.log('hello')
+    const bankName = document.getElementById("bank-name").value;
+    const getAddAmount = parseInt(document.getElementById("add-amount").value);
+    const mainBalance = parseInt(
+      document.getElementById("main-balance").innerText
+    );
+    const accNumber = document.getElementById("acc-number").value;
+    const pin = parseInt(document.getElementById("pin").value);
+    const validPin = 2121;
+
+    // number and pin validation
+    if (accNumber.length != 11) {
+      alert("Account Number Invalid");
+      return;
+    } else if (pin !== validPin) {
+      alert("Your Pin is Invalid");
+      return;
+    }
+
+    // adding calculation
+    const newTotalBalance = mainBalance + getAddAmount;
+    document.getElementById("main-balance").innerText = newTotalBalance;
+  });
