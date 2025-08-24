8be36740d7d3ddcd4b1abfe6e018a62a2a0f0716
AtikHasan16 <akhasannaeem@gmail.com>
2025-08-24 12:08:29 +0600

    fixed

--- script/home.js
+++ script/home.js
@@ -1,21 +1,35 @@
 // Log out button functionality
 document.getElementById("log-out").addEventListener("click", function (event) {
   event.preventDefault();
-  window.location.replace("../index.html");
+  window.location.href = ("../index.html");
 });
+
+function idValue(id) {
+  const idValue = document.getElementById(id).value;
+  return idValue;
+}
+
+function getIdInnerText(id) {
+  const idInnerText = document.getElementById(id).innerText;
+  return idInnerText;
+}
+
+function setInnerText(value) {
+  const mainBalance = (document.getElementById("main-balance").innerText =
+    value);
+  return mainBalance;
+}
 
 document
   .getElementById("add-money")
   .addEventListener("click", function (event) {
     event.preventDefault();
     // console.log('hello')
-    const bankName = document.getElementById("bank-name").value;
-    const getAddAmount = parseInt(document.getElementById("add-amount").value);
-    const mainBalance = parseInt(
-      document.getElementById("main-balance").innerText
-    );
-    const accNumber = document.getElementById("acc-number").value;
-    const pin = parseInt(document.getElementById("pin").value);
+
+    const getAddAmount = parseInt(idValue("add-amount"));
+    const mainBalance = parseInt(getIdInnerText("main-balance"));
+    const accNumber = idValue("acc-number");
+    const pin = parseInt(idValue("pin"));
     const validPin = 2121;
 
     // number and pin validation
@@ -28,10 +42,17 @@ document
     }
 
     // adding calculation
-    const newTotalBalance = mainBalance + getAddAmount;
-    document.getElementById("main-balance").innerText = newTotalBalance;
+    let newTotalBalance = mainBalance + getAddAmount;
+
+    //*** Need support session help
+    // let newMailBalance = idInnerText("main-balance");
+    // newTotalBalance = newMailBalance;
+    // document.getElementById("main-balance").innerText = newTotalBalance;
+    setInnerText(newTotalBalance);
   });
 
+// *** Need support session session help
+// why I can not use function inside this event with styles?
 document.getElementById("add-money-ftr").addEventListener("click", function () {
   document.getElementById("add-money-form").style.display = "block";
   document.getElementById("cashout-form").style.display = "none";
